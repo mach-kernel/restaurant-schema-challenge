0a9df603ce250160ea393e769c9dc9354232e3ac
David Stancu <dstancu@nyu.edu>
2016-08-29 21:25:57 -0400

    add rspec, rubocop, clean up house

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+  # Settings specified here will take precedence over those in
+  # config/application.rb.
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
@@ -24,7 +26,8 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
+  # Asset digests allow you to set far-future HTTP expiration dates
+  # on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
