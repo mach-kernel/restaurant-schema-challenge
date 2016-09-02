74dfb705898b6732888393731a378710fd74a42e
David Stancu <dstancu@nyu.edu>
2016-09-01 23:53:11 -0400

    fix ci script

--- ci_merge.sh
+++ ci_merge.sh
@@ -1,4 +1,7 @@
 [[ `git log -1 --oneline --pretty=%B` =~ ^wip$ ]] && exit 1
 git config --global user.email "dstancu+schema_chlg@nyu.edu"
 git config --global user.name "CircleCI (dstancu)"
-git checkout master && git merge - --no-ff --no-edit && git push
+
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+
+git checkout master && git merge $BRANCH --no-ff --no-edit && git push
