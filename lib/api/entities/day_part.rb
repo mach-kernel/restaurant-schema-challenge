30cd5c8248f45ce14ca577434d9f440efcd9fc5a
David Stancu <dstancu@nyu.edu>
2016-09-03 23:52:22 -0400

    wip: add ot, dp entities, link to location

--- lib/api/entities/day_part.rb
+++ lib/api/entities/day_part.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 module API
   module Entities
-    module Location
+    module DayPart
       include Roar::JSON
       include Roar::Hypermedia
       include Grape::Roar::Representer
@@ -11,9 +11,6 @@ module API
       link :self do |opts|
         Entities.format_link(opts, represented, self.class.name)
       end
-
-      collection :order_types, extend: Entities::OrderType
-      collection :day_parts, extend: Entities::DayPart
     end
   end
 end
