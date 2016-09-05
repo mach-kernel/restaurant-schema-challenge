74dfffb4e280887c9a2e32ea2543e46e30dc55ac
David Stancu <mach@Octal.hubble.lan>
2016-09-05 10:33:25 -0400

    move self/properties into included hook

--- lib/api/entities/base.rb
+++ lib/api/entities/base.rb
@@ -8,18 +8,14 @@ module API
         Grape::Roar::Representer
       ]
 
-      CONST_INCLUDES.each { |c| include c }
-
-
-      property :name
-
-      link :self do |opts|
-        Entities.format_link(opts, represented, self.class.name)
-      end
-
       def self.included(mod)
         mod.module_eval {
           CONST_INCLUDES.each { |c| include c }
+          property :name
+
+          link :self do |opts|
+            Entities.format_link(opts, represented, self.class.name)
+          end
         }
       end
     end
