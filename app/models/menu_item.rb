5b1f49b43ef311f2cbfc5b8350c1cab2e238f2e9
David Stancu <mach@Octal.hubble.lan>
2016-09-05 18:20:47 -0400

    wip

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -6,6 +6,7 @@ class MenuItem
   field :name, type: String
 
   belongs_to :brand
+
   has_many :price_levels, dependent: :destroy do
     def retrieve_pricing(order_type, day_part = nil)
       query = {
